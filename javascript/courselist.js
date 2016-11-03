5ff96fb8042956eae3b8ff6dbc3f4b39023cd9ab
Nvenom <N.venomize@gmail.com>
2016-11-03 17:27:17 -0500

    Create courselist.js

---
+++ javascript/courselist.js
@@ -0,0 +1,1 @@
+var courseList = [    {        name: 'Basic Training',        effect: [            {                name: 'discipline',                percent: '0.2'            },{                name: 'conditioning',                percent: '0.17'            }        ]    }];
