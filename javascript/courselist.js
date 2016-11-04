23d9d213a079c5a074079e95b02d1ad1ea08c09e
Nvenom <N.venomize@gmail.com>
2016-11-04 15:25:05 -0500

    Updated Generators to dynamic list

--- javascript/courselist.js
+++ javascript/courselist.js
@@ -1,10 +1,13 @@
-var courseList = [{
-  name: 'Basic Training',
-  effect: [{
-    name: 'discipline',
-    percent: '0.2'
-  }, {
-    name: 'conditioning',
-    percent: '0.17'
-  }]
-}];
+var courseList = [
+    {
+        name: 'Basic Training',
+        max: '90',
+        effect: [{
+            name: 'discipline',
+            percent: '0.2'
+        }, {
+            name: 'conditioning',
+            percent: '0.17'
+        }]
+    }
+];
