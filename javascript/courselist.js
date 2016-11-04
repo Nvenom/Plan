83ac0c1f087a1bf106f15f25421418f31284449a
Nvenom <N.venomize@gmail.com>
2016-11-03 22:22:27 -0500

    Update courselist.js

--- javascript/courselist.js
+++ javascript/courselist.js
@@ -1,1 +1,10 @@
-var courseList = [    {        name: 'Basic Training',        effect: [            {                name: 'discipline',                percent: '0.2'            },{                name: 'conditioning',                percent: '0.17'            }        ]    }];
+var courseList = [{
+  name: 'Basic Training',
+  effect: [{
+    name: 'discipline',
+    percent: '0.2'
+  }, {
+    name: 'conditioning',
+    percent: '0.17'
+  }]
+}];
