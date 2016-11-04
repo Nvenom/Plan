6d7cf3131dbabf355068106b4f3fdc66fc827b10
Nvenom <N.venomize@gmail.com>
2016-11-04 18:55:14 -0500

    Push

--- javascript/courselist.js
+++ javascript/courselist.js
@@ -2,12 +2,23 @@ var courseList = [
     {
         name: 'Basic Training',
         max: '90',
-        effect: [{
-            name: 'discipline',
-            percent: '0.2'
-        }, {
-            name: 'tactics',
-            percent: '0.17'
-        }]
+        effect: [
+            {
+                name: 'discipline',
+                percent: '1'
+            }, {
+                name: 'tactics',
+                percent: '0.5'
+            }, {
+                name: 'marksmanship',
+                percent: '0.1'
+            }, {
+                name: 'deception',
+                percent: '0.4'
+            }, {
+                name: 'perception',
+                percent: '0.25'
+            }
+        ]
     }
 ];
